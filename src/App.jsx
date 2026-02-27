68459bbb7b7d00b7cab7b51c8185c2b9cf440c36
Mrityunjay jha <mrityunjayjha836@gmail.com>
2026-02-27 16:29:08 +0530

    props updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Cart from './components/Cart'
 function App() {
   return (
     <div className='Parents'>
-      <Cart user='Mj' age={26} img="https://t3.ftcdn.net/jpg/02/38/98/18/360_F_238981836_FMFYp2asxr8PiaGLiowUk6yElg3B0tp5.jpg"/>
-      <Cart user='Ap' age={27} img="https://img.freepik.com/premium-vector/ap-signature-letter-love-logo-with-circle-letter-ap-ap-signature-icon-vector-icon-valentine_930804-5542.jpg"/>
-      <Cart user='APMJ' age={29} img="https://www.shutterstock.com/shutterstock/photos/1590211525/display_1500/stock-vector-m-love-j-letter-logo-or-symbol-template-design-1590211525.jpg"/>
+      <Cart brand='Amazon' date="5 day ago" designation="UX/UI Designer" time="Part-Time" exprence="Seiner" price={120} city="Mumbia" img="https://t3.ftcdn.net/jpg/02/38/98/18/360_F_238981836_FMFYp2asxr8PiaGLiowUk6yElg3B0tp5.jpg"/>
+      <Cart brand='Google' date="30 day ago" designation="Graphic Designer" time="Part-Time" exprence="Seiner" price={150} city="Kochi" img="https://img.freepik.com/premium-vector/ap-signature-letter-love-logo-with-circle-letter-ap-ap-signature-icon-vector-icon-valentine_930804-5542.jpg"/>
+      <Cart brand='Dribble' date="15 day ago" designation="Motion Designer" time="Part-Time" exprence="Seiner" price={75} city="Chennai" img="https://www.shutterstock.com/shutterstock/photos/1590211525/display_1500/stock-vector-m-love-j-letter-logo-or-symbol-template-design-1590211525.jpg"/>
      </div>
   )
 }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,12 +3,34 @@ import React from 'react'
 const Cart = (props) => {
  
   return (
-     <div className='Card' >
+    <>
+    <div className='Card' >
+     <div className='Top-Part' >
       <img src={props.img} alt="" />
-      <h1> {props.user}, {props.age} </h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-      <button>View Profile</button>
+      <p>Save</p>
      </div>
+
+     <div className="mid-part">
+      <h2>{props.brand} <span>{props.date}</span> 
+        </h2>
+      <h1> {props.designation} </h1>
+      <div className='mid-part-button'>
+       <button>{props.time}</button>
+       <button>{props.exprence}</button>
+      </div>
+     </div>
+      <hr />
+     <div className="bottom-part">
+      <div className='bottom-part-para'>
+        <h2>${props.price} /hr</h2>
+        <p>{props.city} India</p>
+      </div>
+      <div className='bottom-part-button'>
+         <button>Apply Now</button>
+      </div>
+     </div>
+</div>
+     </>
   )
 }
 
